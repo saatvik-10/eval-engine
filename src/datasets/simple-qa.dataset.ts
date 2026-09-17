8aef363731bd7e8935369442a524c6b62409ec6d
saatvik-10 <saatvikmadan529@gmail.com>
2026-09-18 00:55:39 +0530

    testing simple dataset

---
+++ src/datasets/simple-qa.dataset.ts
@@ -0,0 +1,12 @@
+import type { EvaluationSample } from '../types/evaluation-sample';
+
+export const simpleQADataset: EvaluationSample<string, string>[] = [
+  {
+    input: 'WHAT IS 2 + 2 ?',
+    expectedOutput: '4',
+  },
+  {
+    input: 'WHO IS THE GREATEST FOOTBALL PLAYER OF ALL TIME ?',
+    expectedOutput: 'Lionel Messi',
+  },
+];
