d2e22187118962ab0ce16a5cb93e9e778f870e6f
saatvik-10 <saatvikmadan529@gmail.com>
2026-09-20 19:01:04 +0530

    added report generation

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { ExactMatchMetric } from './metrics/exact-match.metric';
 import { FakeModelProvider } from './providers/fake.provider';
 import { EvaluationRunner } from './runner/evaluation.runner';
 import { SimpleQATask } from './tasks/simple-qa.task';
+import { ConsoleReporter } from './reports/console.reporter';
 
 const task = new SimpleQATask();
 
@@ -15,5 +16,9 @@ const metric = [new ExactMatchMetric()];
 
 const runner = new EvaluationRunner(task, provider, metric);
 const results = await runner.run(simpleQADataset);
+
+const reporter = new ConsoleReporter();
+
+reporter.report(results);
 
 console.log(JSON.stringify(results, null, 2));
